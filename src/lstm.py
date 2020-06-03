9d558b4a2caa03a53f749ef4d6fb564901d6c24e
jakob-ed <jakob.ed@pm.me>
2020-06-03 09:35:40 +0200

    Add working LSTM DDP example

---
+++ src/lstm.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+from torchtext import data
+from torchtext import datasets
+
+import time
+import random
+import sys
+
+
+USE_PARAMS_FOR_GPU_TRAINING = True
+
+SEED = 1234
+
+random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+TEXT = data.Field(lower = True)  # can have unknown tokens
+UD_TAGS = data.Field(unk_token = None)  # can't have unknown tags
+
+# don't load PTB tags
+fields = (("text", TEXT), ("udtags", UD_TAGS), (None, None))
+
+train_data, valid_data, test_data = datasets.UDPOS.splits(fields)
+
+print(len(train_data), len(valid_data), len(test_data))
+
+print(vars(train_data.examples[1800])['text'])
+print(vars(train_data.examples[1800])['udtags'])
+
+MIN_FREQ = 2
+
+TEXT.build_vocab(train_data,
+                 min_freq = MIN_FREQ)
+
+UD_TAGS.build_vocab(train_data)
+
+BATCH_SIZE = 128
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data),
+    batch_size = BATCH_SIZE,
+    device = device)
+
+INPUT_DIM = len(TEXT.vocab)
+
+if USE_PARAMS_FOR_GPU_TRAINING:
+  EMBEDDING_DIM = 100
+  HIDDEN_DIM = 128
+else:
+  EMBEDDING_DIM = 20
+  HIDDEN_DIM = 32
+
+OUTPUT_DIM = len(UD_TAGS.vocab)
+N_LAYERS = 2
+BIDIRECTIONAL = False
+DROPOUT = 0.25
+PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+
+class BiLSTMPOSTagger(nn.Module):
+    def __init__(self,
+                 input_dim,
+                 embedding_dim,
+                 hidden_dim,
+                 output_dim,
+                 n_layers,
+                 bidirectional,
+                 dropout,
+                 pad_idx):
+
+        super().__init__()
+
+        self.embedding = nn.Embedding(input_dim, embedding_dim, padding_idx = pad_idx)
+
+        self.lstm = nn.LSTM(embedding_dim,
+                            hidden_dim,
+                            num_layers = n_layers,
+                            bidirectional = bidirectional,
+                            dropout = dropout if n_layers > 1 else 0)
+
+        self.fc = nn.Linear(hidden_dim * 2 if bidirectional else hidden_dim, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text):
+
+        #text = [sent len, batch size]
+
+        #pass text through embedding layer
+        embedded = self.dropout(self.embedding(text))
+
+        #embedded = [sent len, batch size, emb dim]
+
+        #pass embeddings into LSTM
+        outputs, (hidden, cell) = self.lstm(embedded)
+
+        #outputs holds the backward and forward hidden states in the final layer
+        #hidden and cell are the backward and forward hidden and cell states at the final time-step
+
+        #output = [sent len, batch size, hid dim * n directions]
+        #hidden/cell = [n layers * n directions, batch size, hid dim]
+
+        #we use our outputs to make a prediction of what the tag should be
+        predictions = self.fc(self.dropout(outputs))
+
+        #predictions = [sent len, batch size, output dim]
+
+        return predictions
+
+def init_weights(m):
+    for name, param in m.named_parameters():
+        nn.init.normal_(param.data, mean = 0, std = 0.1)
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def categorical_accuracy(preds, y, tag_pad_idx):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+    max_preds = preds.argmax(dim = 1, keepdim = True) # get the index of the max probability
+    non_pad_elements = (y != tag_pad_idx).nonzero()
+    correct = max_preds[non_pad_elements].squeeze(1).eq(y[non_pad_elements])
+    return correct.sum() / torch.FloatTensor([y[non_pad_elements].shape[0]])
+
+def run(rank):
+    # create local model
+    model = BiLSTMPOSTagger(INPUT_DIM,
+                        EMBEDDING_DIM,
+                        HIDDEN_DIM,
+                        OUTPUT_DIM,
+                        N_LAYERS,
+                        BIDIRECTIONAL,
+                        DROPOUT,
+                        PAD_IDX).to('cpu')
+
+    # TODO: where should this go!?
+    model.apply(init_weights)
+
+    print(f'The model has {count_parameters(model):,} trainable parameters')
+    model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+    TAG_PAD_IDX = UD_TAGS.vocab.stoi[UD_TAGS.pad_token]
+
+    print('rank ', rank, ' initial: ', sum(parameter.sum() for parameter in model.parameters()))
+    # construct DDP model
+    ddp_model = DDP(model)
+    print('rank ', rank, ' initial: ', sum(parameter.sum() for parameter in ddp_model.parameters()))
+    # define loss function and optimizer
+    criterion = nn.CrossEntropyLoss(ignore_index = TAG_PAD_IDX)
+    # TODO: loss_fn stems from example
+    loss_fn = nn.CrossEntropyLoss(ignore_index = TAG_PAD_IDX)
+    optimizer = optim.Adam(ddp_model.parameters())
+    # TODO: should this stay?
+    optimizer.zero_grad()
+
+    # TODO: send model and criterion to(device) ?
+
+    ddp_model.train()
+    for batch in train_iterator:
+        text = batch.text
+        tags = batch.udtags
+        optimizer.zero_grad()
+        predictions = ddp_model(text)
+        predictions = predictions.view(-1, predictions.shape[-1])
+        tags = tags.view(-1)
+        loss = criterion(predictions, tags)
+        acc = categorical_accuracy(predictions, tags, TAG_PAD_IDX)
+        loss.backward()
+        optimizer.step()
+        break
+
+    print('rank ', rank, ' parameters: ', sum(parameter.sum() for parameter in ddp_model.parameters()))
+    sys.exit(0)
+
+def init_process(fn, backend='mpi'):
+    dist.init_process_group(backend)
+    fn(dist.get_rank())
+
+
+if __name__ == "__main__":
+    init_process(run)
