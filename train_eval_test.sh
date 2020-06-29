bda1a3313dc8d0a6b0dcb8798b2cb8a5463a02ab
pi01 <pi@pi01.hpi.de>
2020-06-29 14:38:35 +0200

    adasum on 8 nodes

--- train_eval_test.sh
+++ train_eval_test.sh
@@ -5,6 +5,6 @@ source ./env.sh
 # echo "$SUSML_DIR_PATH"
 mkdir -p $SUSML_DIR_PATH # has only been created on master so far...
 
-source ~/susml/jakob_jonas/bin/activate && OMPI_MCA_opal_event_include=poll python3 -u ./src/rnn_mpi_ddp.py 2>&1 | tee "$SUSML_DIR_PATH/$OMPI_COMM_WORLD_RANK.out"
+source ~/susml/jakob_jonas/bin/activate && OMPI_MCA_opal_event_include=poll python3 -u ./src/rnn_horovod.py 2>&1 | tee "$SUSML_DIR_PATH/$OMPI_COMM_WORLD_RANK.out"
 
 unset "${!SUSML_@}"
--- wrapper_script.sh
+++ wrapper_script.sh
@@ -9,8 +9,8 @@ cp ./train_eval_test.sh "$SUSML_DIR_PATH/train_eval_test.sh"
 cp ./hostfile "$SUSML_DIR_PATH/hostfile"
 cp ./ray_config.yaml "$SUSML_DIR_PATH/ray_config.yaml"
 
-# horovodrun --hostfile hostfile_no_master_training -np $SUSML_PARALLELISM_LEVEL --mpi-args="--map-by socket:pe=3 -x SUSML_DIR_PATH=$SUSML_DIR_PATH" bash ./train_eval_test.sh
-mpirun -x SUSML_DIR_PATH=$SUSML_DIR_PATH -n $SUSML_PARALLELISM_LEVEL --map-by socket:pe=3 -hostfile ./hostfile --mca orte_fork_agent bash ./train_eval_test.sh
+horovodrun --hostfile hostfile -np $SUSML_PARALLELISM_LEVEL --mpi-args="--map-by socket:pe=3 -x SUSML_DIR_PATH=$SUSML_DIR_PATH" bash ./train_eval_test.sh
+#mpirun -x SUSML_DIR_PATH=$SUSML_DIR_PATH -n $SUSML_PARALLELISM_LEVEL --map-by socket:pe=3 -hostfile ./hostfile --mca orte_fork_agent bash ./train_eval_test.sh
 # horovodrun
 
 unset "${!SUSML_@}"
