f0b036cd929eb723fe2e149cd64cac30bd72a6d7
jakob-ed <jakob.ed@pm.me>
2020-07-22 16:13:08 +0200

    Use git commit hash and diff for reproducibility

--- src/wrapper_script.sh
+++ src/wrapper_script.sh
@@ -1,13 +1,17 @@
 SUSML_TIMESTAMP="$(date +'%m-%d-%Y--%H-%M-%S')"
-SUSML_DIR_PATH="../logs/$SUSML_TIMESTAMP"
+# create log directory outside git repo in case git repo is sshfs-mounted to avoid networking/IO
+SUSML_DIR_PATH="../../logs/$SUSML_TIMESTAMP"
 mkdir -p $SUSML_DIR_PATH
 
+# pipe current commit hash and diff into file for reproducibility
+git rev-parse HEAD > "$SUSML_DIR_PATH/git_commit_hash.txt"
+# make sure untracked files show up in the diff as well / mark them as tracked without staging them https://stackoverflow.com/a/857696
+git add -N *
+git status > "$SUSML_DIR_PATH/git_status.txt"
+git diff > "$SUSML_DIR_PATH/git_diff.txt"
+git diff --staged > "$SUSML_DIR_PATH/git_diff_staged.txt"
+
 source ./env.sh
-cp ./wrapper_script.sh "$SUSML_DIR_PATH/wrapper_script.sh"
-cp ./env.sh "$SUSML_DIR_PATH/env.sh"
-cp ./train_eval_test.sh "$SUSML_DIR_PATH/train_eval_test.sh"
-cp ./hostfile "$SUSML_DIR_PATH/hostfile"
-cp ./ray_config.yaml "$SUSML_DIR_PATH/ray_config.yaml"
 
 horovodrun --hostfile hostfile -np $SUSML_PARALLELISM_LEVEL --mpi-args="--map-by socket:pe=3 -x SUSML_DIR_PATH=$SUSML_DIR_PATH" bash ./train_eval_test.sh
 #mpirun -x SUSML_DIR_PATH=$SUSML_DIR_PATH -n $SUSML_PARALLELISM_LEVEL --map-by socket:pe=3 -hostfile ./hostfile --mca orte_fork_agent bash ./train_eval_test.sh
