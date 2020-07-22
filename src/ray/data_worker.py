0cd5455421d75b11a827f85ab4a3c116af566d04
jakob-ed <jakob.ed@pm.me>
2020-07-22 11:43:07 +0200

    Measure val accuracy between groups of batches

--- src/ray/data_worker.py
+++ src/ray/data_worker.py
@@ -30,6 +30,8 @@ RAND_SEED = int(os.environ['SUSML_RAND_SEED'])
 NUM_EPOCHS = int(os.environ['SUSML_NUM_EPOCHS'])
 BATCH_SIZE = int(os.environ['SUSML_BATCH_SIZE'])
 LR = float(os.environ['SUSML_LR'])
+EVAL_BETWEEN_BATCHES = True if os.environ['SUSML_EVAL_BETWEEN_BATCHES'] == 'true' else False
+EVAL_EVERY_X_BATCHES = int(os.environ['SUSML_EVAL_EVERY_X_BATCHES'])
 # model
 RNN_LAYER_TYPE = os.environ['SUSML_RNN_LAYER_TYPE']
 # distribution
@@ -47,6 +49,7 @@ class DataWorker(object):
         self.rank = rank
         self.epoch_loss = 0
         self.epoch_acc = 0
+        self.batch_idx = 1
 
         TEXT = data.Field(lower = True)  # can have unknown tokens
         UD_TAGS = data.Field(unk_token = None)  # can't have unknown tags
@@ -125,9 +128,12 @@ class DataWorker(object):
 
         try:
             batch = next(self.data_iterator)
+            self.batch_idx += 1
         except StopIteration:  # When the epoch ends, start a new epoch.
+            print(f'starting new epoch on rank {self.get_rank()}')
             self.data_iterator = iter(self.train_iterators[self.rank])
             batch = next(self.data_iterator)
+            self.batch_idx = 1
 
         before = datetime.now()
         text = batch.text
@@ -145,5 +151,30 @@ class DataWorker(object):
         # self.epoch_loss += loss.item()
         # print(f'finished computing gradients on node {self.rank}')
         print(f'computed gradients for a batch on node {self.rank}, took {datetime.now() - before}...')
+
+        # TODO: assert hashes before and after evaluating of model.get_gradients() are the same!
+
+        if EVAL_BETWEEN_BATCHES and (batch_idx % EVAL_EVERY_X_BATCHES == 0):
+            epoch_loss = 0
+            epoch_acc = 0
+
+            # TODO: set model.train() somewhere else before applying workers' gradients!?
+            self.model.eval()
+
+            with torch.no_grad():
+                for batch in self.valid_iterator:
+                    text = batch.text
+                    tags = batch.udtags
+                    predictions = self.model(text)
+                    predictions = predictions.view(-1, predictions.shape[-1])
+                    tags = tags.view(-1)
+                    loss = self.criterion(predictions, tags)
+                    acc = self.categorical_accuracy(predictions, tags)
+                    epoch_loss += loss.item()
+                    epoch_acc += acc.item()
+
+            valid_loss, valid_acc = (epoch_loss / len(self.valid_iterator), epoch_acc / len(self.valid_iterator))
+            print(f'evaluating after batch {self.batch_idx} on rank {self.get_rank()}: Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
+            self.model.train()
 
         return self.model.get_gradients()
