564c847ad49f56bdc08d1058c65d6f376377040f
jakob-ed <jakob.ed@pm.me>
2020-07-31 12:19:40 +0200

    Make MPI ready

--- src/env.sh
+++ src/env.sh
@@ -11,8 +11,8 @@ export SUSML_LR=0.001
 #  linear scaling rule for batch_size <-> lr ...
 # export SUSML_BATCH_SIZE=512
 # export SUSML_LR=0.004
-export SUSML_EVAL_BETWEEN_BATCHES=true
+export SUSML_EVAL_BETWEEN_BATCHES=false
 export SUSML_EVAL_EVERY_X_BATCHES=2
 
 # distribution / mpi / horovod
-export SUSML_PARALLELISM_LEVEL=2
+export SUSML_PARALLELISM_LEVEL=12
--- src/train_eval_test.sh
+++ src/train_eval_test.sh
@@ -4,11 +4,17 @@ source ./src/env.sh
 # has only been created on master so far...
 mkdir -p $SUSML_DIR_PATH
 
+echo "this is hostname $(hostname)"
 if [[ $(hostname) == pi* ]]
 then
     echo "am on pi cluster"
     source ~/susml/jakob_jonas/bin/activate
 fi
-OMPI_MCA_opal_event_include=poll python3 -u ./src/horovod/rnn_horovod.py 2>&1 | tee "$SUSML_DIR_PATH/$OMPI_COMM_WORLD_RANK.out"
+
+# MPI:
+OMPI_MCA_opal_event_include=poll python3 -u ./src/mpi_ddp/rnn_mpi_ddp.py 2>&1 | tee "$SUSML_DIR_PATH/$OMPI_COMM_WORLD_RANK.out"
+
+# Horovod:
+# OMPI_MCA_opal_event_include=poll python3 -u ./src/horovod/rnn_horovod.py 2>&1 | tee "$SUSML_DIR_PATH/$OMPI_COMM_WORLD_RANK.out"
 
 unset "${!SUSML_@}"
